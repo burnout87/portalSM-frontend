6390d3656a70e8e2d81cc0a1731513e2b57fa02c
burnout87 <gabriele.barni@gmail.com>
2020-12-30 21:13:04 +0100

    le bestemmie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from "./app-routing.module";
+import { ModalModule } from './modal/modal.module';
 import { AppComponent } from "./app.component";
 import { MachineFormComponent } from "./machine-form/machine-form.component";
 import { ConnectivityService } from "./connectivity.service";
 import { HttpClientModule } from "@angular/common/http";
-import { MachineComponent } from './machine/machine.component';
+import { DialogContentUserDialog, MachineComponent } from './machine/machine.component';
 import { MaterialModule } from './material.modules';
 import { ListMachineComponent } from './list-machine/list-machine.component';
 import { FileUploadModule } from "ng2-file-upload";
@@ -23,10 +24,11 @@ import { OwerSearchComponent } from './ower-search/ower-search.component';
 import { SearchMachineComponent } from './search-machine/search-machine.component';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { CapitalizeFirstPipe } from './capitalize-first.pipe';
+import { TooltipModule } from '@syncfusion/ej2-angular-popups';
 
 @NgModule({
-  declarations: [AppComponent, MachineFormComponent, MachineComponent, ListMachineComponent, FileUploadComponent, LoginComponent, OwnerFormComponent, OwerSearchComponent, SearchMachineComponent, CapitalizeFirstPipe],
-  imports: [BrowserModule, GalleryModule.forRoot(), BrowserAnimationsModule, NgxUsefulSwiperModule, CommonModule, ReactiveFormsModule, FormsModule, AppRoutingModule, FileUploadModule, MaterialModule, HttpClientModule, ColorPickerModule],
+  declarations: [AppComponent, MachineFormComponent, DialogContentUserDialog, MachineComponent, ListMachineComponent, FileUploadComponent, LoginComponent, OwnerFormComponent, OwerSearchComponent, SearchMachineComponent, CapitalizeFirstPipe],
+  imports: [TooltipModule, ModalModule, BrowserModule, GalleryModule.forRoot(), BrowserAnimationsModule, NgxUsefulSwiperModule, CommonModule, ReactiveFormsModule, FormsModule, AppRoutingModule, FileUploadModule, MaterialModule, HttpClientModule, ColorPickerModule],
   providers: [ConnectivityService],
   bootstrap: [AppComponent],
 })
--- src/app/machine/machine.component.ts
+++ src/app/machine/machine.component.ts
@@ -1,30 +1,53 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Inject, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Machine } from '../machine';
-import { SwiperOptions, Swiper } from 'swiper/bundle';
-import { SwiperComponent } from 'ngx-useful-swiper';
+import { Owner } from '../owner';
 import { ConnectivityService } from '../connectivity.service';
 import { Description, DescriptionStrategy } from '@ks89/angular-modal-gallery';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MAT_DIALOG_SCROLL_STRATEGY } from '@angular/material/dialog';
+import { Overlay, ScrollStrategy } from '@angular/cdk/overlay';
+
+// export function scrollFactory(overlay: Overlay): () => ScrollStrategy {
+//   return () => overlay.scrollStrategies.reposition();
+// }
 
 @Component({
   selector: 'app-machine',
   templateUrl: './machine.component.html',
-  styleUrls: ['./machine.component.css']
+  styleUrls: ['./machine.component.css'],
+  // providers: [
+  //   { 
+  //     provide: MAT_DIALOG_SCROLL_STRATEGY, 
+  //     useFactory: scrollFactory, 
+  //     deps: [Overlay] 
+  //   }
+  // ]
 })
 export class MachineComponent implements OnInit, AfterViewInit  {
 
-  constructor(private csService: ConnectivityService) { }
+  constructor(private csService: ConnectivityService,
+    public dialog: MatDialog,
+    private overlay: Overlay) {
+     }
 
   @Output() machineRemovedEvent: EventEmitter<Machine> = new EventEmitter();
   @Input() machineData: Machine;
   @Input() id: Number;
-  // @ViewChild('galleryTop',{static: false}) galleryTop: SwiperComponent;
-  // @ViewChild('galleryThumbs',{static: false}) galleryThumbs: SwiperComponent;
+  // @ViewChild('userDataTooltip') public userDataTooltip: TooltipComponent;
+  ownerDataDialog: MatDialogRef<DialogContentUserDialog, any>;
+  tooltipContent: string = "";
 
   private slideThumb:boolean = true;
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    // this.tooltipContent = '<ng-template #content>\
+    //       <h3>User profile</h3>\
+    //       <p><strong>Name</strong> ' + this.machineData.ownerData?.name + '\
+    //       <p><strong>Surname</strong> ' + this.machineData.ownerData?.surname + '\
+    //     </ng-template>';
+  }
 
-   ngAfterViewInit() { }
+  ngAfterViewInit() {  }
 
   deleteMachine() {
     this.csService.RemoveMachine(this.machineData._id).subscribe(response => {
@@ -33,7 +56,74 @@ export class MachineComponent implements OnInit, AfterViewInit  {
     });
   }
 
+  // tooltip/dialog stuff
+  openDialog(event) {
+    
+    if(this.machineData.ownerData) {
+      const bottom = event.target.getBoundingClientRect().bottom - (event.target.getBoundingClientRect().height / 2);
+      const left  = event.target.getBoundingClientRect().left + event.target.getBoundingClientRect().width;
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.backdropClass = 'custom-dialog-backdrop-class';
+      dialogConfig.panelClass = 'custom-dialog-panel-class';
+      dialogConfig.hasBackdrop = true;
+      dialogConfig.scrollStrategy = this.overlay.scrollStrategies.close();
+      // dialogConfig.disableClose = true;
+      // dialogConfig.autoFocus = true;
+      dialogConfig.data = this.machineData.ownerData
+      dialogConfig.position = {
+        top:  bottom + 'px',
+        left: left + 'px'
+      };
+      dialogConfig.width = '600px' ;
+      this.ownerDataDialog = this.dialog.open(
+        DialogContentUserDialog, 
+        dialogConfig
+       );
+  
+      this.ownerDataDialog.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+    }
+  }
+
+  // beforeClose(args: TooltipEventArgs) {
+    // console.log(args.type);
+    // if(!args.target)
+    // args.cancel = true;
+  // }
+
+  // openModal(event) {
+  //   this.modalService.open(this.machineData._id.toString(), event.clientX, event.clientY);
+  // }
+
+  // closeModal(event) {
+  //   this.modalService.close(this.machineData._id.toString());
+  // }
+
   customDescription: Description = {
     strategy: DescriptionStrategy.HIDE_IF_EMPTY
   };
+}
+
+@Component({
+  selector: 'dialog-content-user-dialog',
+  templateUrl: 'dialog-content-user-dialog.html',
+  styleUrls: ['./dialog-content-user-dialog.css'],
+})
+export class DialogContentUserDialog {
+
+  constructor(
+    public thisDialogRef: MatDialogRef<DialogContentUserDialog, any>, 
+    @Inject(MAT_DIALOG_DATA) public ownerData: Owner) { }
+
+  ngOnInit() {
+  }
+
+  onCloseConfirm() {
+    this.thisDialogRef.close('Confirm');
+  }
+
+  onCloseCancel() {
+    this.thisDialogRef.close('Cancel');
+  }
 }

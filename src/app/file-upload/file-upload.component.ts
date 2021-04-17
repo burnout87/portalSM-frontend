9a519f9362a2fb6faff20f510c79b61a9f4d3838
burnout87 <gabriele.barni@gmail.com>
2021-04-17 18:28:02 +0200

    code clean-up

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -26,26 +26,6 @@ export class FileUploadComponent implements OnInit, ControlValueAccessor  {
     this.inputFiles = this.router.getCurrentNavigation().extras.state?.images as Array<any>;
   }
 
-  private dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    let byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {type:mimeString});
-  }
-
   private dataURLtoFile(dataurl, filename) {
   
     var arr = dataurl.split(','),
